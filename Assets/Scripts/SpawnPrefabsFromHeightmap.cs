6e09354de1b0e59d18d2ea5f8cb8d4eab47d5a90
johnasharifi <johnasharifi@gmail.com>
2020-09-20 18:42:29 -0700

    Add events to generate cubes on each tile
    Each tile will generate a cube.
    
    Need to implement a mesh-combiner for heightmap

---
+++ Assets/Scripts/SpawnPrefabsFromHeightmap.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Heightmap))]
+public class SpawnPrefabsFromHeightmap : MonoBehaviour
+{
+    [SerializeField] private Heightmap heightmap;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Dictionary<int, MaterialPropertyBlock> biomeMatBlocks = new Dictionary<int, MaterialPropertyBlock>();
+        foreach (int key in heightmap.colorLookupTable.Keys)
+        {
+            Color c = heightmap.colorLookupTable[key];
+
+            Texture2D tex = new Texture2D(1, 1);
+            tex.SetPixel(0, 0, c);
+            tex.Apply();
+
+            biomeMatBlocks[key] = new MaterialPropertyBlock();
+            biomeMatBlocks[key].SetTexture("_MainTex", tex);
+        }
+        
+        heightmap.onBiomesGenerated += (int biome, int x, int z, Color c) =>
+        {
+            GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            go.transform.position = new Vector3(x, 0f, z);
+            go.GetComponent<Renderer>().SetPropertyBlock(biomeMatBlocks[biome]);
+            go.transform.SetParent(heightmap.transform);
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
