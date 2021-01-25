419ae5b8ced91dbf0cf8130a9fee32d10f4d1ae6
johnasharifi <johnasharifi@gmail.com>
2021-01-24 21:07:29 -0800

    Have submesh filters set their UVs and grid scale by Heightmap scale
    Need to draw textures on our submeshes. This requires that submeshes have UV coordinates. This requires that we know heightmap scale.
    
    Need to draw grid scale. This requires that we know heightmap scale.
    
    So we need to know the heightmap!

--- Assets/Scripts/SubmeshFilter.cs
+++ Assets/Scripts/SubmeshFilter.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 /// <summary>
 /// Class for storing the biome associated with a submesh.
@@ -8,6 +9,27 @@ using UnityEngine;
 [RequireComponent(typeof(MeshFilter))]
 public class SubmeshFilter : MonoBehaviour
 {
+    [SerializeField] private Heightmap m_Heightmap;
+    public Heightmap Heightmap
+    {
+        get
+        {
+            if (m_Heightmap == null) m_Heightmap = GetComponentInParent<Heightmap>();
+            return m_Heightmap;
+        }
+    }
+
+    [SerializeField] private Renderer m_Renderer;
+    public Renderer Renderer
+    {
+        get
+        {
+            if (m_Renderer == null) m_Renderer = GetComponent<Renderer>();
+            return m_Renderer;
+        }
+    }
+    
+    const float gridTranslate = 0.5f;
     [SerializeField] private int m_biome;
     public int biome
     {
@@ -18,6 +40,17 @@ public class SubmeshFilter : MonoBehaviour
         get
         {
             return m_biome;
+        }
+    }
+
+    private void OnEnable()
+    {
+        if (Heightmap != null)
+        {
+            // apply a translation to grid
+            MeshFilter mf = GetComponent<MeshFilter>();
+            Renderer.material.SetTextureScale("_MainTex", Vector2.one * Heightmap.getDim(0));
+            mf.mesh.SetUVs(0, mf.mesh.vertices.ToList().Select(x => new Vector2(x.x - gridTranslate, x.y - gridTranslate) / Heightmap.getDim(0)).ToList());
         }
     }
 }
