5ded779b45beaacd16bc5292639ff38898734972
johnasharifi <johnasharifi@gmail.com>
2021-06-27 22:13:24 -0700

    Implement MapWithData
    Implement 2D array of name-float pairs to store data in. This will be the basis of a lot of our data storage

---
+++ Assets/Scripts/MapWithData.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A 2D array of name-float pairs. This data structure will store core game info in a way that is relatively sparse.
+/// </summary>
+public class MapWithData : MonoBehaviour
+{
+	private class FloatLookup : Dictionary<string, float> { };
+
+	const int dimensions = 100;
+
+	FloatLookup[,] data = new FloatLookup[dimensions, dimensions];
+
+	public float this[int i, int j, string name] {
+		get {
+			if (data[i, j] == null) {
+				data[i, j] = new FloatLookup();
+			}
+
+			if (!data[i, j].ContainsKey(name)) {
+				data[i, j][name] = default;
+			}
+
+			return data[i, j][name];
+		}
+	}
+}
