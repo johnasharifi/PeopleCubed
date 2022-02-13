dce29a86ffa673375f207518297baa7db2330f66
johnasharifi <johnasharifi@gmail.com>
2022-02-13 00:00:03 -0800

    Add MapBuilding which is a berry bush
    Berry bush could be on "berries" recipe, which consumes nothing and periodically emits berries

--- Assets/Scripts/MapBuilding.cs
+++ Assets/Scripts/MapBuilding.cs
@@ -4,4 +4,24 @@ using UnityEngine;
 
 public class MapBuilding : MapUnit
 {
+	readonly WaitForSeconds waiter = new WaitForSeconds(1f);
+
+	[SerializeField] private List<Recipe> activeRecipes;
+
+	private void Start()
+	{
+		StartCoroutine(ManageRecipes());
+	}
+
+	IEnumerator ManageRecipes()
+	{
+		for (; ; )
+		{
+			foreach (Recipe recipe in activeRecipes)
+			{
+			}
+
+			yield return waiter;
+		}
+	}
 }
