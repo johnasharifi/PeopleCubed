4633cd71900e7358e8ad9cbfc2139c23f32b37eb
johnasharifi <johnasharifi@gmail.com>
2022-02-12 23:56:06 -0800

    Add basic Recipe data-tracking
    Recipe can have input cost(s), benefit(s), and effects

---
+++ Assets/Scripts/Recipe.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct Recipe
+{
+	[SerializeField] private List<InventoryItemCost> costs;
+	[SerializeField] private List<InventoryItemCost> benefits;
+	
+	[SerializeField] private List<AbstractEffect> effectOnManufactureTick;
+	[SerializeField] private List<AbstractEffect> effectOnBeginManufacture;
+	[SerializeField] private List<AbstractEffect> effectOnFinishManufacture;
+
+	[SerializeField] private float manufactureTickCount;
+}
+
+[System.Serializable]
+public struct InventoryItemCost {
+	public InventoryItems item;
+	public float cost;
+}
+
+public enum InventoryItems {
+	GRAIN,
+	VEGETABLES,
+
+	BEER,
+	WINE,
+
+	WOOD,
+	STONE,
+	BRICK,
+
+	CLAY,
+	POTTERY,
+
+	SPICE,
+
+	COPPER,
+	IRON,
+	SILVER,
+	GOLD,
+
+
+}
